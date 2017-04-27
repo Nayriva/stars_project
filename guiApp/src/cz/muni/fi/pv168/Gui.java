f1936cf91b698da40d40ac84085ed65ab62043af
Dominik František Bučík <xbucik@mail.muni.cz>
2017-04-27 10:12:23 +0200

    New diagrams, started gui design

---
+++ guiApp/src/cz/muni/fi/pv168/Gui.java
@@ -0,0 +1,21 @@
+package cz.muni.fi.pv168;
+
+import javax.swing.*;
+
+/**
+ * Created by nayriva on 27.4.2017.
+ */
+public class Gui {
+    private JPanel panel1;
+    private JPanel agentPane;
+    private JTable agentTable;
+    private JButton addNewAgentButton;
+    private JButton editSelectedAgentButton;
+    private JButton deleteSelectedAgentButton;
+    private JButton listAllAgentsButton;
+    private JButton listAgentsButton;
+    private JRadioButton aliveRadioButton;
+    private JRadioButton deadRadioButton;
+    private JButton listAgentsButton1;
+    private JSpinner spinner1;
+}
